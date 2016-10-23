e2d1132f983f59ec39b0862c0ca602ffce787af3
Tim Strehle <tim@strehle.de>
2016-10-23 19:53:04 +0000

    A first test for occurrences.

---
+++ tests/Model/OccurrenceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use TopicCards\Interfaces\iTopicMap;
+
+
+class OccurrenceTest extends TestCase
+{
+    /** @var \TopicCards\Interfaces\iTopicMap */
+    protected static $topicmap;
+
+    
+    public static function setUpBeforeClass()
+    {
+        global $topicmap;
+        
+        self::$topicmap = $topicmap;
+    }
+    
+    
+    public function testNewTypeAndDatatype()
+    {
+        $occurrence_type = 'http://example.com/schema/newOccurrenceType';
+        $datatype = 'http://example.com/schema/newDatatype';
+        
+        $topic = self::$topicmap->newTopic();
+        
+        $occurrence = $topic->newOccurrence();
+        
+        $occurrence->setType($occurrence_type);
+        $occurrence->setDatatype($datatype);
+        $occurrence->setValue('hello world');
+        
+        $ok = $topic->save();
+        
+        $this->assertGreaterThanOrEqual(0, $ok, 'Topic save failed');
+        
+        $ok = $topic->load($topic->getId());
+
+        $this->assertGreaterThanOrEqual(0, $ok, 'Topic load after save failed');
+        
+        $occurrence = $topic->getFirstOccurrence([ 'type' => $occurrence_type ]);
+        
+        $expected =
+            [
+                'value' => 'hello world',
+                'reifier' => false,
+                'scope' => [ ]
+            ];
+
+        $occurrence_data = $occurrence->getAll();
+        
+        $this->assertArraySubset($expected, $occurrence_data);
+        
+        $occurrence_type_topic = self::$topicmap->newTopic();
+        $ok = $occurrence_type_topic->load($occurrence->getTypeId());
+        $this->assertGreaterThanOrEqual(0, $ok, 'Occurrence type topic load failed');
+        
+        $this->assertContains
+        (
+            iTopicMap::SUBJECT_OCCURRENCE_TYPE, 
+            $occurrence_type_topic->getTypes(), 
+            'New occurrence type topic has not been marked as occurrence type.'
+        );
+
+        $datatype_topic = self::$topicmap->newTopic();
+        $ok = $datatype_topic->load($occurrence->getDatatypeId());
+        $this->assertGreaterThanOrEqual(0, $ok, 'Datatype topic load failed');
+
+        $this->assertContains
+        (
+            iTopicMap::SUBJECT_DATATYPE,
+            $datatype_topic->getTypes(),
+            'New datatype has not been marked as datatype.'
+        );
+    }
+}
