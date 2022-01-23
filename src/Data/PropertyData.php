45cf09c30455717406e00a505e44cd6898184be0
Tim Strehle <tim.strehle@spiegel.de>
2022-01-23 23:12:36 +0100

    Add property type constants

--- src/Data/PropertyData.php
+++ src/Data/PropertyData.php
@@ -5,6 +5,13 @@ namespace StrehleDe\TopicCards\Data;
 
 class PropertyData
 {
+    const TYPE_INTEGER = 'integer';
+    const TYPE_FLOAT = 'float';
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_DATE = 'date';
+    const TYPE_TIME = 'time';
+    const TYPE_DATETIME = 'datetime';
+
     protected string $name = '';
     protected string $type = '';
     protected array $values = [];
